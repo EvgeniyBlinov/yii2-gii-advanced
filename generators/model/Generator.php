423afedb9a654f130493f4f34370fddc88748f5e
Blinov Evgeniy <eblinov@alvioneurope.ru>
2015-04-07 18:38:28 +0300

    master Work stage 2015-04-07 18:38:28 A  generators/model/Generator.php A  generators/model/default/model.php A  generators/model/default/modelBase.php A  generators/model/form.php

---
+++ generators/model/Generator.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace common\etc\gii_custom\templates\model;
+
+use Yii;
+use yii\gii\CodeFile;
+
+class Generator extends \yii\gii\generators\model\Generator
+{
+    /**
+     * @var string
+     **/
+    public $ns        = 'common\models';
+
+    /**
+     * @var string
+     **/
+    public $baseClass = '\common\models\Base';
+
+    /**
+     * @return array of required templates
+     */
+    public function requiredTemplates()
+    {
+        return ['model.php','modelBase.php'];
+    }
+
+    /**
+     * Generate
+     *
+     * @return array
+     */
+    public function generate()
+    {
+        $files     = [];
+        $relations = $this->generateRelations();
+        $db        = $this->getDbConnection();
+        foreach ($this->getTableNames() as $tableName) {
+            $className   = $this->generateClassName($tableName);
+            $tableSchema = $db->getTableSchema($tableName);
+            $params      = [
+                'tableName'   => $tableName,
+                'className'   => $className,
+                'tableSchema' => $tableSchema,
+                'labels'      => $this->generateLabels($tableSchema),
+                'rules'       => $this->generateRules($tableSchema),
+                'relations'   => isset($relations[$className]) ? $relations[$className] : [],
+            ];
+            $files[] = new CodeFile(
+                Yii::getAlias('@' . str_replace('\\', '/', $this->ns)) . '/' . lcfirst($className) . '/' . $className . 'Base.php',
+                $this->render('modelBase.php', $params)
+            );
+            $files[] = new CodeFile(
+                Yii::getAlias('@' . str_replace('\\', '/', $this->ns)) . '/' . lcfirst($className) . '/' . $className . '.php',
+                $this->render('model.php', $params)
+            );
+        }
+
+        return $files;
+    }
+
+    /**
+     * Generates validation rules for the specified table.
+     * @param \yii\db\TableSchema $table the table schema
+     * @return array the generated validation rules
+     */
+    public function generateRules($table)
+    {
+        if (isset($table->columns) && is_array($table->columns)) {
+            $excludeFromRequiredRule = ['created_date', 'updated_date'];
+            foreach ($excludeFromRequiredRule as $columnName) {
+                if (array_key_exists($columnName, $table->columns)) {
+                    $table->columns[$columnName]->allowNull = true;
+                }
+            }
+        }
+        return parent::generateRules($table);
+    }
+}
---
+++ generators/model/default/model.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * This is the template for generating the model class of a specified table.
+ */
+
+/* @var $this yii\web\View */
+/* @var $generator yii\gii\generators\model\Generator */
+/* @var $tableName string full table name */
+/* @var $className string class name */
+/* @var $tableSchema yii\db\TableSchema */
+/* @var $labels string[] list of attribute labels (name => label) */
+/* @var $rules string[] list of validation rules */
+/* @var $relations array list of relations (name => relation declaration) */
+
+echo "<?php\n";
+?>
+
+namespace <?= $generator->ns . '\\' . lcfirst($className) ?>;
+
+use Yii;
+
+/**
+ * This is the model class for table "<?= $generator->generateTableName($tableName) ?>".
+ */
+class <?= $className ?> extends <?= $className ?>Base
+{
+
+}
